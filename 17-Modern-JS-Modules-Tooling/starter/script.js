fec1f9b9eb0a5c12ffe7694f7cdfc5df2739d584
Abuemkeze Chu <abuemkeze@gmail.com>
2023-09-04 15:25:32 +0100

    the module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -9,12 +9,36 @@ console.log(totalPrice, totalQuantity);
 // const data = await response.json()
 // console.log(data);
 
-const getLastPost = async () => {
-  const response = await fetch('https://jsonplaceholder.org/posts')
-  const data = await response.json()
+// const getLastPost = async () => {
+//   const response = await fetch('https://jsonplaceholder.org/posts')
+//   const data = await response.json()
 
-  return { title: data.at(-1).title, text: data.at(-1).content }
-}
+//   return { title: data.at(-1).title, text: data.at(-1).content }
+// }
 
-const lastPost = await getLastPost()
-console.log(lastPost);
+// const lastPost = await getLastPost()
+// console.log(lastPost);
+
+const shoppingCard2 = (function () {
+  const cart = []
+  const shippingCost = 10
+  const totalPrice = 237
+  const totalQuantity = 23
+
+  const addToCart = (product, quantity) => {
+    cart.push({ product, quantity })
+    console.log(`${quantity} ${product} added to cart`);
+  }
+
+  const orderStock = (product, quantity) => {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  }
+
+  return { addToCart, cart, totalPrice, totalQuantity }
+
+})()
+
+shoppingCard2.addToCart('apple', 4)
+console.log(shoppingCard2);
+console.log(shoppingCard2.shippingCost);
+
