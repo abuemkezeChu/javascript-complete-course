7a3ee5965f9ac6fd64c93507194b9109d71095a0
Abuemkeze Chu <abuemkeze@gmail.com>
2023-09-04 15:53:38 +0100

    working with external modules and npm

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,34 +1,33 @@
 import { addToCart, totalPrice, totalQuantity } from './shoppingCard.js'
 
-console.log('Importing Module');
-addToCart('Banana', 5)
-console.log(totalPrice, totalQuantity);
+// console.log('Importing Module');
+// addToCart('Banana', 5)
+// console.log(totalPrice, totalQuantity);
 
+/*
+const response = await fetch('https://jsonplaceholder.org/posts')
+const data = await response.json()
+console.log(data);
 
-// const response = await fetch('https://jsonplaceholder.org/posts')
-// const data = await response.json()
-// console.log(data);
+const getLastPost = async () => {
+  const response = await fetch('https://jsonplaceholder.org/posts')
+  const data = await response.json()
 
-// const getLastPost = async () => {
-//   const response = await fetch('https://jsonplaceholder.org/posts')
-//   const data = await response.json()
+  return { title: data.at(-1).title, text: data.at(-1).content }
+}
 
-//   return { title: data.at(-1).title, text: data.at(-1).content }
-// }
+const lastPost = await getLastPost()
+console.log(lastPost);
+*/
 
-// const lastPost = await getLastPost()
-// console.log(lastPost);
-
+/*
 const shoppingCard2 = (function () {
   const cart = []
   const shippingCost = 10
   const totalPrice = 237
   const totalQuantity = 23
 
-  const addToCart = (product, quantity) => {
-    cart.push({ product, quantity })
-    console.log(`${quantity} ${product} added to cart`);
-  }
+  const  
 
   const orderStock = (product, quantity) => {
     console.log(`${quantity} ${product} ordered from supplier`);
@@ -41,4 +40,20 @@ const shoppingCard2 = (function () {
 shoppingCard2.addToCart('apple', 4)
 console.log(shoppingCard2);
 console.log(shoppingCard2.shippingCost);
+*/
 
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js'
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 }
+  ],
+  user: { loggedIn: true },
+}
+
+const stateClone = Object.assign({}, state)
+const stateCloneDeep = cloneDeep(state)
+state.user.loggedIn = false
+console.log(stateClone);
+console.log(stateCloneDeep);
